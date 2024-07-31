91f5bebabbd6435eb0e897fe3bea1cbf1c5ed724
sassanghz <sassanghazi@gmail.com>
2024-07-31 17:45:15 -0600

    db server

---
+++ website/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+const app = express();
+const port = 3000;
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+//Connect to the db;
+mongoose.connect('mongodb://localhost:27017/emailDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+const emailSchema = new mongoose.Schema({
+    email: String,
+});
+
+const Email = mongoose.model('Email', emailSchema);
+
+app.post('/submit-email', (req, res) => {
+    
+    const newEmail = new Email({
+        email: req.body.email,
+    });
+
+    newEmail.save((err) => {
+        if(err) {
+            res.status(500).send('Error saving email');
+        }else{
+            res.status(200).json({ message: 'Email saved successfully' });
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log('Server running on http://localhost:${port}');
+})
