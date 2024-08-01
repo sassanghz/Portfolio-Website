f1668b9b07e8e7fd669ab55ac27460e68cf88ce5
sassanghz <sassanghazi@gmail.com>
2024-07-31 18:27:07 -0600

    depricate options

--- server.js
+++ server.js
@@ -3,17 +3,15 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-
 const app = express();
 const port = 3000;
 
-
+// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
-//Connect to the db;
-mongoose.connect('mongodb://localhost:27017/emailDB', {useNewUrlParser: true, useUnifiedTopology: true});
-
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/emailDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const emailSchema = new mongoose.Schema({
     email: String,
@@ -22,20 +20,20 @@ const emailSchema = new mongoose.Schema({
 const Email = mongoose.model('Email', emailSchema);
 
 app.post('/submit-email', (req, res) => {
-    
     const newEmail = new Email({
         email: req.body.email,
     });
 
     newEmail.save((err) => {
-        if(err) {
+        if (err) {
             res.status(500).send('Error saving email');
-        }else{
+        } else {
+            console.log(`Email saved: ${req.body.email}`); // Log email to the console
             res.status(200).json({ message: 'Email saved successfully' });
         }
     });
 });
 
 app.listen(port, () => {
-    console.log('Server running on http://localhost:${port}');
-})
+    console.log(`Server running on http://localhost:${port}`);
+});
